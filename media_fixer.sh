55f2d53d1453ba453b5ce0bd7c0fdc4c49210f18
root <root@server.casa>
2024-02-23 14:21:00 +0100

    Set disclaimer

--- media_fixer.sh
+++ media_fixer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# By Willy Gardiol, provided under the GPLv3 License. https://www.gnu.org/licenses/gpl-3.0.html
+# Publicly available at: https://github.com/gardiol/media_fixer
+# You can contact me at willy@gardiol.org
 
 TEST_ONLY=0
 DEBUG=1
