e66bbbfa6651cce246cde958e1e3826e25ab8777
Willy Gardiol <willy@gardiol.org>
2024-10-31 08:50:22 +0100

    Improving queuing capabilities

--- media_fixer.sh
+++ media_fixer.sh
@@ -98,6 +98,85 @@ function parse_mediainfo_output
 	fi
 }
 
+function preprocess_video_file()
+{
+	local full_filename="$*"
+
+	local l_result=2 # 0= failed, 1= success, 2= skipped
+	local l_change_container=0
+	local l_encode=0
+	local l_resize=0
+
+	print_notice "Analyzing file '${full_filename}'..."
+
+	parse_mediainfo_output "${full_filename}" "General" "Format"
+	if [ $? -eq 0 ]
+	then
+		if [ "${mediainfo_value}" != "${CONTAINER}" ]
+		then
+			print_notice "Container needs to be converted from '${mediainfo_value}' to '${CONTAINER}'..."
+			l_change_container=1
+		else 
+			print_notice "Container already '${CONTAINER}'."
+		fi
+
+		parse_mediainfo_output "${full_filename}" "Video" "Format"
+		if [ $? -eq 0 ]
+		then
+			if [ "${mediainfo_value}" != "${VIDEO_CODEC}" ]
+			then
+				print_notice "Movie needs to be encoded from '${mediainfo_value}' to '${VIDEO_CODEC}'..."
+				l_encode=1
+			else 
+				print_notice "Video already at '${VIDEO_CODEC}' encoding."
+			fi
+			parse_mediainfo_output "${full_filename}" "Video" "Height"
+			mediainfo_value="${mediainfo_value% *}"
+			if [ $? -eq 0 ]
+			then
+				# remove blanks inside height string (since mediainfo will report 1080 as "1 080"):
+				mediainfo_value=${mediainfo_value//[[:space:]]/}
+				if [ "${mediainfo_value}" != "${VIDEO_HEIGHT}" ]
+				then
+					if [ ${mediainfo_value} -gt ${VIDEO_HEIGHT} ]
+					then
+						print_notice "Movie needs to be resized from '${mediainfo_value}' to '${VIDEO_HEIGHT}'..."
+						l_resize=1
+					else
+						print_notice "Not resizing upward: '${mediainfo_value}' is smaller than '${VIDEO_HEIGHT}'."
+					fi
+				else 
+					print_notice "Video already at '${VIDEO_HEIGHT}' resolution."
+				fi
+			else
+				print_error "Unable to parse Video Height"
+				l_result=0
+			fi
+		else
+			print_error "Unable to parse Video Format"
+			l_result=0
+		fi
+	else
+		print_error "Unable to parse General Format"
+		l_result=0
+	fi
+
+	if [ $l_result -eq 0 ]
+	then
+		print_notice "   Video is invalid or corrupted and cannot be processed."
+	else
+		if [ $l_change_container -eq 1 -o $l_encode -eq 1 -o $l_resize -eq 1 ]
+		then
+			l_result=1
+			print_notice "   Video needs to be processed."
+		fi
+	fi
+	export result=$l_result
+	export change_container=$l_change_container
+	export encode=$l_encode
+	export resize=$l_resize
+}
+
 
 ######### Begin of script #############
 
@@ -110,9 +189,9 @@ fi
 
 create_queue=0
 queue_file="processing_queue"
-if [ -e ${queue_file} ]
+if [ -e ${queue_file}.in_progress ]
 then
-	line=$(head -n 1 ${queue_file})
+	line=$(head -n 1 ${queue_file}.in_progress)
 	if [ "${line}" = "" ]
 	then
 		create_queue=1
@@ -125,96 +204,98 @@ if [ ${create_queue} -eq 1 ]
 then
 	# Scan folders / subfolders and file files...
 	print_notice "Calculating queues..."
-	for j in skipped failed completed
+	for j in skipped failed completed in_progress temp
 	do
-		test -e ${queue_file}.${j} && rm ${queue_file}.{$j}
+		test -e ${queue_file}.${j} && rm ${queue_file}.${j}
 	done
-	test -e ${queue_file} && rm ${queue_file}
 
 	find . -type f -exec file -N -i -- {} + | sed -n 's!: video/[^:]*$!!p' | {
 	while read line
 	do
-		echo ${line} >> ${queue_file}
+		# temp files end with ".working", those needs to be ignored
+		is_temp=${line%working}
+		if [ "${line%working}" = "${line}" ]
+		then
+			echo ${line} >> ${queue_file}.temp
+		else
+			print_notice "Skipping file '${line}' because it seems a temporary file, you should maybe delete it?"
+		fi
 	done
 	}
 fi
 
-print_notice "Queue has "$(cat ${queue_file} | wc -l)" videos to be processed..."
-read discard
+print_notice "Queue has "$(cat ${queue_file}.temp | wc -l)" videos to be analyzed..."
 
-line=$(head -n 1 ${queue_file})
+# Iterate all files...
+line=$(head -n 1 ${queue_file}.temp)
 while [ "${line}" != "" ]
 do
-	result=2 # 0= failed, 1= success, 2= skipped
-
+	result=0
 	change_container=0
 	encode=0
 	resize=0
+	preprocess_video_file "${line}"
 
-	full_filename=${line}
+	# Remove file from queue...
+	tail -n +2 ${queue_file}.temp > ${queue_file}.cleaned
+	mv ${queue_file}.cleaned ${queue_file}.temp
+
+	# Move file to appropriate new queue
+	if [ $result -eq 0 ]
+	then
+		# add file to failed queue
+		print_notice "Video '${line}' added to failed queue"
+		echo ${line} >> ${queue_file}.failed
+	elif [ $result -eq 2 ]
+	then
+		# add file to skipped queue
+		print_notice "Video '${line}' added to skipped queue"
+		echo ${line} >> ${queue_file}.skipped
+	elif [ $result -eq 1 ]
+	then
+		# add file to process queue
+		print_notice "Video '${line}' added to processing queue (${change_container} ${encode} ${resize})"
+		echo "${line}|||| ${change_container} ${encode} ${resize}" >> ${queue_file}.in_progress
+	else
+		print_notice "Invalid value of '$result' in result!"
+	fi
+	line=$(head -n 1 ${queue_file}.temp)
+done
+
+test -e ${queue_file}.failed || touch ${queue_file}.failed
+test -e ${queue_file}.skipped || touch ${queue_file}.skipped
+test -e ${queue_file}.in_progress || touch ${queue_file}.in_progress
+
+print_notice "Failed queue has "$(cat ${queue_file}.failed | wc -l)" videos."
+print_notice "Skipped queue has "$(cat ${queue_file}.skipped | wc -l)" videos."
+print_notice "Work queue has "$(cat ${queue_file}.in_progress | wc -l)" videos to be processed..."
+
+
+# Iterate all files...
+line=$(head -n 1 ${queue_file}.in_progress)
+while [ "${line}" != "" ]
+do
+	result=0
+
+	full_filename=${line%||||*}
 	filepath="${full_filename%/*}"
 	filename="${full_filename##*/}"
 	extension="${filename##*.}"
 	stripped_filename="${filename%.*}"
 	
-	print_notice "Analyzing file '${full_filename}'..."
+	temp=${line#*||||}
+	change_container=${temp%[[:space:]][[:digit:]][[:space:]][[:digit:]]}
+	encode=${temp%[[:space:]][[:digit:]]}
+	encode=${encode#[[:space:]][[:digit:]][[:space:]]}
+	resize=${temp##[[:space:]][[:digit:]][[:space:]][[:digit:]][[:space:]]}
 
-	parse_mediainfo_output "${full_filename}" "General" "Format"
-	if [ $? -eq 0 ]
-	then
-		if [ "${mediainfo_value}" != "${CONTAINER}" ]
-		then
-			print_notice "Container needs to be converted from '${mediainfo_value}' to '${CONTAINER}'..."
-			change_container=1
-		else 
-			print_notice "Container already '${CONTAINER}'."
-		fi
-	else
-		print_error "Unable to parse General Format"
-	fi
-
-
-	parse_mediainfo_output "${full_filename}" "Video" "Format"
-	if [ $? -eq 0 ]
-	then
-		if [ "${mediainfo_value}" != "${VIDEO_CODEC}" ]
-		then
-			print_notice "Movie needs to be encoded from '${mediainfo_value}' to '${VIDEO_CODEC}'..."
-			encode=1
-		else 
-			print_notice "Video already at '${VIDEO_CODEC}' encoding."
-		fi
-	else
-		print_error "Unable to parse Video Format"
-	fi
-
-	parse_mediainfo_output "${full_filename}" "Video" "Height"
-	mediainfo_value="${mediainfo_value% *}"
-	if [ $? -eq 0 ]
-	then
-		# remove blanks inside height string (since mediainfo will report 1080 as "1 080"):
-		mediainfo_value=${mediainfo_value//[[:space:]]/}
-		if [ "${mediainfo_value}" != "${VIDEO_HEIGHT}" ]
-		then
-			if [ ${mediainfo_value} -gt ${VIDEO_HEIGHT} ]
-			then
-				print_notice "Movie needs to be resized from '${mediainfo_value}' to '${VIDEO_HEIGHT}'..."
-				resize=1
-			else
-				print_notice "Not resizing upward: '${mediainfo_value}' is smaller than '${VIDEO_HEIGHT}'."
-			fi
-		else 
-			print_notice "Video already at '${VIDEO_HEIGHT}' resolution."
-		fi
-	else
-		print_error "Unable to parse Video Height"
-	fi
+	echo "Processing: '$full_filename'..."
 
 	if [ $change_container -eq 1 -o $encode -eq 1 -o $resize -eq 1 ]
 	then
 		result=0
 		print_notice "   Video needs to be processed."
-		(
+		my_cwd="$PWD"
 		print_notice "Relocating to path '${filepath}' for easier operations..."
 		if cd "${filepath}"
 		then
@@ -234,7 +315,7 @@ do
 					exec_command mv "${intermediate_filename}" "${working_filename}" &>> "${LOG_FILE}"
 				else
 					print_error "Transmux failed!"
-					exec_command rm -f "${intermediate_filename}" &>> "${LOG_FILE}"
+					exec_command rm -f "${intermediate_filename}"
 					error=1
 				fi
 			fi # transmux
@@ -258,14 +339,14 @@ do
 						ffmpeg_options="${ffmpeg_options} ${FFMPEG_RESIZE}"
 					fi
 
-					exec_command ffmpeg -fflags +genpts -nostdin -i "${source_filename}" ${ffmpeg_options} "${intermediate_filename}" &>> "${LOG_FILE}"
+					exec_command ffmpeg -fflags +genpts -nostdin -i "${source_filename}" ${ffmpeg_options} "${intermediate_filename}"
 					if [ $? -eq 0 ]
 					then
 						print_notice "Encoding ok."
-						exec_command mv "${intermediate_filename}" "${working_filename}" &>> "${LOG_FILE}"
+						exec_command mv "${intermediate_filename}" "${working_filename}"
 					else
 						print_error "Encoding failed!"
-						exec_command rm -f "${intermediate_filename}" &>> "${LOG_FILE}"
+						exec_command rm -f "${intermediate_filename}"
 						error=1
 					fi
 				fi # encore or resize
@@ -277,14 +358,14 @@ do
 				then
 					destination_filename="${stripped_filename}.${CONTAINER_EXTENSION}"
 					print_notice "Moving final product from '${working_filename}' to '${destination_filename}'..."
-					exec_command mv "${working_filename}" "${destination_filename}" &>> "${LOG_FILE}"
+					exec_command mv "${working_filename}" "${destination_filename}"
 					if [ $? -eq 0 ]
 					then
 						result=1
 						if [ "${filename}" != "${destination_filename}" ]
 						then
 							print_notice "Removing original file..."
-							exec_command rm -f "${filename}" &>> "${LOG_FILE}"
+							exec_command rm -f "${filename}"
 						else
 							print_notice "Original file has been replaced with converted file."
 						fi
@@ -297,27 +378,25 @@ do
 			else
 				print_error "Something went wrong in conversion."
 			fi
+			cd "$my_cwd"
 		else
 			print_error "Unable to cd to '${filepath}'"
 		fi 
-		) 
+		
 	fi # change container or encode
 
-	print_notice "Moving processed file from processing queue..."
+	print_notice "Removing processed file from processing queue..."
 	if [ ${result} -eq 1 ]
 	then
 		echo ${line} >> ${queue_file}.completed
-	elif [ ${result} -eq 2 ]
-	then
-		echo ${line} >> ${queue_file}.skipped
 	else
-		echo ${line} >> ${queue_file}.failed
+		echo ${full_filename} >> ${queue_file}.failed
 	fi
 
 	# remove from queue
-	tail -n +2 ${queue_file} > ${queue_file}.cleaned
-	mv ${queue_file}.cleaned ${queue_file}
-	line=$(head -n 1 ${queue_file})
+	tail -n +2 ${queue_file}.in_progress > ${queue_file}.cleaned
+	mv ${queue_file}.cleaned ${queue_file}.in_progress
+	line=$(head -n 1 ${queue_file}.in_progress)
 
 done
 
